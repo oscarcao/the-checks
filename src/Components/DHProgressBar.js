1498ea082c5ad480fbe8bbfbee23b5af8bfb6ec8
Oscar Cao <oscarcao@OSC-MacBookProRetina.local>
2018-08-21 13:38:35 +1000

    Minor formatting changes

--- src/Components/DHProgressBar.js
+++ src/Components/DHProgressBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default ({ currentValue, maxValue}) => (             
+export default ({ currentValue, maxValue }) => (             
     <div className="dh-progressbar">
         <div className="dh-progressbar__filler" style={{ width: `${currentValue/maxValue * 100}%` }} />
     </div>
